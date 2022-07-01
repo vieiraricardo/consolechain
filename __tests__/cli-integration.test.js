d8994dad247a3dc092a69dff3919398c38b5ee80
vieiraricardo <hi@ricvieira.com>
2022-06-30 23:33:25 -0300

    fix bin path

--- __tests__/cli-integration.test.js
+++ __tests__/cli-integration.test.js
@@ -3,7 +3,7 @@ const { system, filesystem } = require('gluegun')
 const src = filesystem.path(__dirname, '..')
 
 const cli = async (cmd) =>
-  system.run('node ' + filesystem.path(src, 'bin', 'chainteract') + ` ${cmd}`)
+  system.run('node ' + filesystem.path(src, 'bin', 'consolechain') + ` ${cmd}`)
 
 test('outputs version', async () => {
   const output = await cli('--version')
