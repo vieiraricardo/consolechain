009c53b6a3938c557dac3e88c1f66ad6064e69d8
vieiraricardo <hi@ricvieira.com>
2022-12-14 19:25:28 -0300

    chore: add support for goerli network

--- src/lib/rpc.js
+++ src/lib/rpc.js
@@ -1,12 +1,12 @@
 module.exports = {
   ethereum: 'https://mainnet.infura.io/v3',
-  rinkeby: 'https://rinkeby.infura.io/v3',
+  goerli: 'https://goerli.infura.io/v3',
   polygon: 'https://polygon-rpc.com',
   mumbai: 'https://rpc-mumbai.maticvigil.com	',
   syscoin: 'https://rpc.syscoin.org',
   explorer: {
     ethereum: 'https://etherscan.io',
-    rinkeby: 'https://rinkeby.etherscan.io',
+    goerli: 'https://goerli.etherscan.io',
     polygon: 'https://polygonscan.com',
     mumbai: 'https://mumbai.polygonscan.com',
     syscoin: 'https://explorer.syscoin.org',
