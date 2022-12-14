ad6898c8b6792f5bf115909ac3b8661beb1ae021
vieiraricardo <hi@ricvieira.com>
2022-12-14 19:40:35 -0300

    fix: goerli rpc

--- src/lib/rpc.js
+++ src/lib/rpc.js
@@ -1,6 +1,6 @@
 module.exports = {
   ethereum: 'https://mainnet.infura.io/v3',
-  goerli: 'https://goerli.infura.io/v3',
+  goerli: 'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161',
   polygon: 'https://polygon-rpc.com',
   mumbai: 'https://rpc-mumbai.maticvigil.com	',
   syscoin: 'https://rpc.syscoin.org',
